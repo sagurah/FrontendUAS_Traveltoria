2870dd7c9f5d5a934a4581429c0a968f731bb23b
sagurah <satyogag@gmail.com>
2022-12-17 15:36:12 +0700

    Update DashboardLayout.vue

--- src/components/Dashboard/DashboardLayout.vue
+++ src/components/Dashboard/DashboardLayout.vue
@@ -10,7 +10,10 @@
                 </v-list-item-content>
             </v-list-item>
             <v-divider></v-divider>
-            <v-list nav>
+            <v-list class="mt-2" nav>
+                <v-chip class="mx-3" color="teal" style="color:white;">
+                    <span><strong>Menus</strong></span>
+                </v-chip>
                 <v-list-item class="my-2" v-for="menu in menus" :key="menu.title" link tag="router-link" :to="menu.to">
                     <v-list-item-icon>
                         <v-icon color="teal darken-3">{{ menu.icon }}</v-icon>
@@ -19,12 +22,38 @@
                         <v-list-item-title style="color:#26A69A; font-size: 120%; font-weight: 600;">{{ menu.title }}</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-                <v-spacer></v-spacer>
-                <v-list-item>
-                    <v-list-item-content>
-                        <!-- <v-list-item-title>test</v-list-item-title> -->
-                    </v-list-item-content>
-                </v-list-item>
+                <div class="mt-16">
+                    <v-chip class="mx-3" color="blue darken-3" style="color:white;">
+                        <span><strong>Akun</strong></span>
+                    </v-chip>
+                    <v-list-item class="my-2" v-for="menu in accountMenus" :key="menu.title" link tag="router-link" :to="menu.to">
+                        <v-list-item-icon>
+                            <v-icon color="blue darken-3">{{ menu.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title style="color:#1565C0; font-size: 120%; font-weight: 600;">{{ menu.title }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn width="100%" small color="red darken-1" style="color:white;" @click="dialogLogout = true">Logout</v-btn>
+                        <v-dialog v-model="dialogLogout" transition="dialog-top-transition" max-width="450">
+                            <v-card style="border-radius: 5px;">
+                                <v-card-title>
+                                    <span class="ma-1"><strong>Konfirmasi</strong></span>
+                                    <span class="pa-1" style="background-color:#F44336; color:white;"><strong>Logout</strong></span>
+                                </v-card-title>
+                                <v-divider></v-divider>
+                                <v-card-text class="mt-2">
+                                    <span style="font-weight:600;">Apakah anda yakin untuk melakukan logout?</span>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn @click="dialogLogout=false">Batal</v-btn>
+                                    <v-btn color="red" style="color:white;">Logout</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </v-list-item>
+                </div>
             </v-list>
         </v-navigation-drawer>
         <v-app-bar app fixed height="80%" color="#FFFF">
@@ -46,13 +75,23 @@ export default {
     name: "DashboardLayout",
     data() {
         return {
+            dialogLogout: false,
             drawer: true,
             menus: [
                 { title: 'Kereta', icon: 'mdi-train', to: '/kereta' },
                 { title: 'Pesawat', icon: 'mdi-airplane', to: '/pesawat' },
                 { title: 'Bus', icon: 'mdi-bus', to: '/bus' },
             ],
+
+            accountMenus: [
+                { title: 'Akun Saya', icon: 'mdi-account', to: '/akun' },
+            ]
         }
     },
+    methods: {
+        logout(){
+            
+        }
+    }
 }
 </script>
