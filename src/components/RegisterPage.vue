f6fcf428c4c57b5e37750d0855fa85eb27e79402
just4fun147 <pauluswindito1@gmail.com>
2022-12-20 00:50:14 +0700

    Register

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -17,24 +17,26 @@
                         <v-card class="pa-1">
                             <v-card-title class="font-weight-bold">Daftar akun baru</v-card-title>
                             <v-card-subtitle>Buat perjalanan anda terasa nyaman dan aktual bersama Traveltoria</v-card-subtitle>
-                            <v-container>
-                                <v-select :items="type" required v-model="form.type" outlined color="teal" label="Tipe"></v-select>
-                                <label>Nama Anda</label>
-                                <v-text-field type="text" label="Name" placeholder="name" v-model="form.name" solo clearable required></v-text-field>
-                                <label>Alamat Email</label>
-                                <v-text-field type="text" label="Email" placeholder="email" v-model="form.email" solo clearable required></v-text-field>
-                                <!-- <label>Gambar Profil</label>
-                                <v-text-field type="file" label="Picture" placeholder="picture" v-model="form.type" accept="image/*" class="input-file" solo clearable required></v-text-field> -->
-                                <label>Kata Sandi</label>
-                                <v-text-field type="password" label="Password" placeholder="password" v-model="form.password" solo clearable required></v-text-field>
-                            </v-container>
-                            <v-card-actions>
-                                <v-btn block color="primary" elevation="2" small x-small> Daftar</v-btn>
-                            </v-card-actions>
+                            <v-form v-model="valid" ref="form">
+                                <v-container>
+                                    <v-select :items="type" required v-model="form.tipe" outlined color="teal" label="Tipe"></v-select>
+                                    <label>Nama Anda</label>
+                                    <v-text-field type="text" label="Name" placeholder="name" v-model="form.name" solo clearable required></v-text-field>
+                                    <label>Alamat Email</label>
+                                    <v-text-field type="text" label="Email" placeholder="email" v-model="form.email" :rules="emailRules" solo clearable required></v-text-field>
+                                    <!-- <label>Gambar Profil</label>
+                                    <v-text-field type="file" label="Picture" placeholder="picture" v-model="form.type" accept="image/*" class="input-file" solo clearable required></v-text-field> -->
+                                    <label>Kata Sandi</label>
+                                    <v-text-field type="password" label="Password" placeholder="password" v-model="form.password" :rules="passwordRules" solo clearable required></v-text-field>
+                                </v-container>
+                                <v-card-actions>
+                                    <v-btn block color="primary" elevation="2" small x-small @click="submit">Daftar</v-btn>
+                                </v-card-actions>
+                            </v-form>
                             <v-card-subtitle>Sudah memiliki akun?<v-btn @click="movepage('/login')" plain color="#00008B" small x-small text>Masuk</v-btn> </v-card-subtitle>
                         </v-card>
                     </v-col>
-                     <v-snackbar v-model="snackbar" :color="color" timeout="2000" bottom>
+                    <v-snackbar v-model="snackbar" :color="color" timeout="2000" bottom>
                          {{ error_message }}
                     </v-snackbar>
                 </v-row>
@@ -44,44 +46,72 @@
 </template>
 <script>
     export default{
-        data(){
-            return{
-                type:[
-                    {text: "Customer", value:1},
-                    {text: "Maskapai Pesawat", value:2},
-                    {text: "Agen Kereta", value:3},
-                    {text: "Agen Bus", value:4}
-                ],
-                form: {
-                    name: '',
-                    type: '',
-                    password: '',    
-                    email: '',
-                },
-            }
+        data () {
+        return {
+            load: false,
+            form: {
+                email: '',
+                password: '',
+                tipe: null,
+                name: '',
+            },
+            type:[
+                {text: "Customer", value:1},
+                {text: "Maskapai Pesawat", value:2},
+                {text: "Maskapai Kereta", value:3},
+                {text: "Agen Bus", value:4},
+            ],
+            snackbar: false,
+            valid: false,
+            error_message: "",
+            emailRules: [(v) => !!v || "E-mail tidak boleh kosong dan format harus valid"],
+            passwordRules: [(v) => !!v || "Password tidak boleh kosong"],
+          
+        };
+    },
+    setup() {
+
+    },
+    methods: {
+        movepage(link) {
+            this.$router.push(link) 
+            this.window.location.reload()
         },
-        // methods:{
-        //     register(){
-        //         var url = this.$api + '/login';
-        //     this.$http.post(url, {
-        //         params:{
-        //             naeme : this.form.name,
-        //             type : this.form.type,
-        //             password : this.form.password,
-        //             email : this.form.email
-        //         }
-        //          headers: {
-        //          'Authorization' : 'Bearer ' + localStorage.getItem('token')
-        //          }
-        //      })
-        //     then(response => {
-        //          this.Buses = response.data.data;
-        //          this.load =true
-        //          this.dialogShow = true
-        //      })
-        //     }
-        // }
+        submit() {
+            if (this.$refs.form.validate()) {
+                this.load = true;
+                this.$http
+                    .post(this.$api + '/registers', {
+                        email : this.form.email,
+                        password : this.form.password,
+                        type: this.form.tipe,
+                        name: this.form.name
+                    // headers: {
+                    // 'Authorization' : 'Bearer ' + localStorage.getItem('token')
+                    // }
+                }).then((response)=> {
+                    // localStorage.setItem("id", response.data.user.id);
+                    this.error_message = response.data.message;
+                    this.color = "green"
+                    this.clear()
+                    this.snackbar = true;
+                    this.$router.push({
+                        name: "DashboardMain",
+                    });
+                })
+                .catch((error)=>{
+                    this.error_message = error.response.data.message;
+                    this.snackbar = true;
+                    this.color = "red"
+                });
+            }
+            
+        },
+        clear() {
+            this.$refs.form.reset();
+        },
     }
+}
 </script>
 <style>
 *{
