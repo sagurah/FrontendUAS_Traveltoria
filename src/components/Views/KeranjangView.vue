f45043ad50bec7e2173a1305a6fc2ad4776b7024
just4fun147 <pauluswindito1@gmail.com>
2022-12-21 13:36:26 +0700

    CRUD TICKET

--- src/components/Views/KeranjangView.vue
+++ src/components/Views/KeranjangView.vue
@@ -7,32 +7,42 @@
                 </v-card-title>
                 <v-divider color="teal" class="mx-4"></v-divider>
                 <v-card-content>
+                    <v-combobox
+                        v-model="select"
+                        :items="searchItem"
+                        label="Search"
+                        single
+                        outlined
+                        dense
+                    ></v-combobox>
                     <v-data-table 
                     color="teal lighten-5" 
                     class="my-3 mx-6 elevation-2" 
                     :headers="headers"
-                    loading
-                    loading-text="Mohon tunggu..."
-                    :items="data"
+                    :items="filter"
                     item-key="nama"
                     >
+                    
                         <template v-slot:[`item.aksi`]="{item}">
-                            <v-btn outlined small @click="checkout(item)" color="teal">
-                                <v-icon left color="white">mdi-cart</v-icon>
-                                <span style="color: white;">Checkout</span>
+                            <span v-if="item.status=='Dipesan'">
+                                <v-btn outlined small @click="checkoutItem(item.id)" color="blue">
+                                <v-icon left color="blue">mdi-cart</v-icon>
+                                <span style="color: blue;">Checkout</span>
                             </v-btn>
-                            <v-btn outlined small @click="deleteItem(item)" color="teal">
-                                <v-icon left color="white">mdi-trashcan</v-icon>
-                                <span style="color: white;">Batalkan</span>
+                            <v-btn outlined small @click="deleteItem(item.id)" color="red">
+                                <v-icon left color="red">mdi-trashcan</v-icon>
+                                <span style="color: red;">Batalkan</span>
                             </v-btn>
+                    </span>
+                            
                         </template>
                     </v-data-table>
                 </v-card-content>
                 <v-card-actions>
-                    <v-btn class="mx-4 mb-2" color="teal">
+                    <!-- <v-btn class="mx-4 mb-2" color="teal">
                         <v-icon left color="white">mdi-cart</v-icon>
                         <span style="color: white;" class="py-2">Checkout semua</span>
-                    </v-btn>
+                    </v-btn> -->
                 </v-card-actions>
             </v-card>
         </v-container>
@@ -42,18 +52,16 @@
 export default{
     data(){
         return{
+            select: "Pesawat",
+            searchItem: [
+                "Pesawat", "Kereta","Bus"
+            ],
             headers: [
-                {
-                    text: "Nama",
-                    align: "start",
-                    sortable: true,
-                    value: "",
-                },
-                { text: "Asal", value: "" },
-                { text: "Kelas", value: "" },
-                { text: "Tujuan", value: "" },
-                { text: "Jadwal Berangkat", value: ""},
-                { text: "Jadwal Tiba", value: ""},
+                { text: "Nama",align: "start", sortable: true, value: "nama"},
+                { text: "Asal", value:"asal"},
+                { text: "Tujuan", value: "tujuan" },
+                { text: "Jadwal Berangkat", value: "jadwal_keberangkatan"},
+                { text: "Jadwal Tiba", value: "jadwal_tiba"},
                 { text: "Aksi", value: "aksi" },
             ],
             data: [],
@@ -63,29 +71,106 @@ export default{
         }
     },
     methods:{
-        readData(){
-            this.$http.get(this.$api + '/ticketKereta', {
-            }).then(response => {
-                this.data = response.data.data;
-            })
-
-            this.$http.get(this.$api + '/ticketBus', {
-            }).then(response => {
-                this.keretas = response.data.data;
-            })
-
-            this.$http.get(this.$api + '/ticketPesawat', {
-            }).then(response => {
-                this.keretas = response.data.data;
-            })
+        deleteItem(id){
+            if(this.select=="Pesawat"){
+                this.$http.delete(this.$api + `/ticketPesawats/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete pesawat'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
+            if(this.select=="Kereta"){
+                this.$http.delete(this.$api + `/ticketKeretas/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete kereta'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
+            if(this.select=="Bus"){
+                this.$http.delete(this.$api + `/ticketBuses/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete bus'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
             
         },
+        checkoutItem(id){
+            if(this.select=="Pesawat"){
+                this.$http.post(this.$api + `/ticketPesawat/lunas/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete pesawat'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
+            if(this.select=="Kereta"){
+                this.$http.post(this.$api + `/ticketKereta/lunas/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete kereta'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
+            if(this.select=="Bus"){
+                this.$http.post(this.$api + `/ticketBus/lunas/${id}`)
+                .then(response => {
+                    this.error_message = '\nBerhasil delete bus'
+                     //this.snackbar = true
+                    this.error_message = response.data.message
+                    this.reload()
+                })
+            }
+        },
+        reload(){
+                setTimeout(() => this.isHidden = false, 2000);
+                window.location.reload();
+            },
     },
+
     mounted() {
+        this.$http.get(this.$api + `/ticketKereta/${localStorage.getItem("id")}`, {
+            }).then(response => {
+                this.keretas = response.data.data;
+            })
+
+            
+
+            this.$http.get(this.$api + `/ticketPesawat/${localStorage.getItem("id")}`, {
+            }).then(response => {
+                this.pesawats = response.data.data;
+            })
+            this.$http.get(this.$api + `/ticketBus/${localStorage.getItem("id")}`, {
+            }).then(response => {
+                this.buses = response.data.data;
+            })
         if(localStorage.getItem("verified")==0){
             this.$router.push('/verif');
         }
         this.clear()
+    },
+    computed:{
+        filter(){
+            if(this.select=="Pesawat"){
+                return this.pesawats
+            }
+            if(this.select=="Kereta"){
+                return this.keretas
+            }
+            if(this.select=="Bus"){
+                return this.buses
+            }
+            return this.pesawats
+        }
+        
     }
 }
 </script>
